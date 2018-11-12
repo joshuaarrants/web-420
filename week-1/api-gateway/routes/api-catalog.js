8e93f6ed0bf7c306c62f5c566fb24822fa1b1d69
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-11-12 14:41:50 -0600

    adding login/logout request

--- week-1/api-gateway/routes/api-catalog.js
+++ week-1/api-gateway/routes/api-catalog.js
@@ -11,4 +11,10 @@ router.post('/auth/register', auth_controller.user_register);
 
 // GET request for verifying user tokens
 router.get('/auth/token', auth_controller.user_token);
+
+// POST request for login request
+router.post('/auth/login', auth_controller.user_login);
+
+// GET request for user logout request
+router.get('/auth/logout', auth_controller.user_logout);
 module.exports = router;
