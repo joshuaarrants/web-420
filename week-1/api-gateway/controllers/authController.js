ad0c3b609740083a0a7fa07785709bb32fab4bfd
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-11-27 15:55:38 -0600

    adding api gateway part v code

--- week-1/api-gateway/controllers/authController.js
+++ week-1/api-gateway/controllers/authController.js
@@ -4,7 +4,7 @@ var bcrypt = require('bcryptjs');
 var config = require('../config');
 
 // Register a new user on POST
-exports.user_register = function(req, res) {
+exports.user_register = function (req, res) {
 
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
@@ -18,61 +18,47 @@ exports.user_register = function(req, res) {
         if (err)
             return res.status(500).send('There was a problem registering the user.');
 
-        var token = jwt.sign({ id: user._id}, config.web.secret, {
+        var token = jwt.sign({ id: user._id }, config.web.secret, {
             expiresIn: 86400 // 24 hours
         });
 
-        res.status(200).send({auth: true, token: token });
+        res.status(200).send({ auth: true, token: token });
     });
 };
 
 // Verify token on GET
 exports.user_token = function (req, res) {
-    var token = req.headers['x-access-token'];
+    User.getById(req.userId, function (err, user) {
+        if (err)
+            return res.status(500).send('There was a problem finding the user.');
 
-    if (!token) {
-        return res.status(401).send({auth: false, message: 'No token provided'});
-    }
+        if (!user)
+            return res.status(404).send('No user found.');
 
-    jwt.verify(token, config.web.secret, function (err, decoded) {
-        if (err) {
-            return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
-        }
-
-        User.getById(decoded.id, function (err, user) {
-            if (err) {
-                return res.status(500).send('There was a problem finding the user.');
-            }
-
-            if (!user) {
-                return res.status(404).send('No user found.');
-            }
-
-            res.status(200).send(user);
-        });
+        res.status(200).send(user);
 
     });
 };
 
 //handles user login request
-exports.user_login = function(req, res){
-    User.getOne(req.body.email, function(err, user) {
+exports.user_login = function (req, res) {
+    User.getOne(req.body.email, function (err, user) {
         if (err) return res.status(500).send('Error on server.');
         if (err) return res.status(404).send('No user found.');
 
         var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
-        if (!passwordIsValid) return res.status(401).send({auth: false, token: null});
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
 
-        var token = jwt.sign({id: user._id}, config.web.secret, {
+        var token = jwt.sign({ id: user._id }, config.web.secret, {
             expiresIn: 86400 //expires in 24 hours
         });
 
-        res.status(200).send({auth: true, token: token});
+        res.status(200).send({ auth: true, token: token });
     })
 };
 
 //handle user logout requests
-exports.user_logout = function(req, res) {
-    res.status(200).send({auth: false, token: null});
+exports.user_logout = function (req, res) {
+    res.status(200).send({ auth: false, token: null });
 };
--- week-1/api-gateway/routes/api-catalog.js
+++ week-1/api-gateway/routes/api-catalog.js
@@ -5,12 +5,13 @@
 var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
+var checkToken = require('../check-token');
 
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 // POST request for login request
 router.post('/auth/login', auth_controller.user_login);
